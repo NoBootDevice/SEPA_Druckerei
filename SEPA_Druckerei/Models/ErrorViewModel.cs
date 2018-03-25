d420f6e3ed058c9816f06c05f017bc363567d40d
Dustin Bär <dustin@Dustins-MacBook-Air.local>
2018-03-25 11:12:54 +0200

    dustin: init commit

---
+++ SEPA_Druckerei/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SEPA_Druckerei.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
